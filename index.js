aade9a33ff5cc51f5846647f12d3cd80f6570c27
muhammadbilal127 <mbilal52257@gmail.com>
2025-01-16 00:23:35 +0500

    loop

---
+++ index.js
@@ -0,0 +1,12 @@
+var cities = [
+    "Karachi", "Lahore", "Faisalabad", "Rawalpindi", "Multan", 
+    "Hyderabad", "Gujranwala", "Peshawar", "Quetta", "Islamabad", 
+    "Sialkot", "Bahawalpur", "Sukkur", "Larkana", "Mingora", 
+    "Sahiwal", "Dera Ghazi Khan", "Mirpur Khas", "Chiniot", "Okara"
+  ];
+  
+  for (var i = 0; i > cities.length; i++) {
+    if (cities[i] === "Islamabad") {alert("islamabad is the cleanist city");
+    }
+  }
+  
